b368e28074b8f1101fcd193705934606f62cff79
ducusinabdul <ducusinabdul@gmail.com>
2024-03-23 20:30:22 +0800

    final

--- MotorPH/src/main/java/COMPONENTS/Credits.java
+++ MotorPH/src/main/java/COMPONENTS/Credits.java
@@ -4,6 +4,9 @@ public class Credits {
     
     public static void CCTO() {
         
+        System.out.println("********");
+        System.out.println("The greatest reward a person can achive from his toil is NOT WHAT HE GETS, BUT WHAT HE BECOMES by it.");
+	System.out.println("********");
         System.out.println("CREDITS:");
         System.out.println(" ");
         System.out.println("-------------------------------------");
@@ -32,6 +35,49 @@ public class Credits {
         System.out.println("Role: \t\tSystem Architect, Workflow Officer, Peace Keeper");
         System.out.println("Contribution: \tEditEmployeeDetails");
         System.out.println("Total Manpower:\t42hrs");
+        System.out.println(" ");
+        System.out.println("Weekly tasks and worked hour/s duration of Group 4:");
+        System.out.println(" ");
+        System.out.println("Week 4");
+        System.out.println("Login page: 2024/01/24 03:00 hrs");
+        System.out.println("Employee's, Admin Landing Page (Time-in, Time-out): 2024/01/24 02:00 hrs");
+        System.out.println("Employee's  Personal Detail's Page (View): 2024/01/25 02:30 hrs ");
+        System.out.println("Employee's Time Log and Salary Detail's Page (View): 2024/01/26 03:00 hrs");
+        System.out.println("Admin's Add and Edit Employee Page: 2024/01/27 03:00 hrs");
+        System.out.println("Admin's View Employee's Detail and Salary Page: 2024/01/28 02:00 hrs");
+        System.out.println("Total worked day/hours: 5 days/15:30 hours");
+        System.out.println(" ");
+        System.out.println("Week 5");
+        System.out.println("Code Login Page (Validate User's Login): 2024/02/06 03:00 hrs");
+        System.out.println("Code Employee's, Admin Landing Page (Time-in, Time-out): 2024/02/09 02:00 hrs ");
+        System.out.println("Code Employee's  Personal Detail's Page (View): 2024/02/10 03:00 ");
+        System.out.println("Code Employee's Time Log and Salary Detail's Page (View): 2024/02/12 02:00");
+        System.out.println("Code Admin's Add and Edit Employee Page: 2024/02/14 03:00");
+        System.out.println("Code Admin's View Employee's Detail and Salary Page: 2024/02/15 02:00");
+        System.out.println("Total worked day/hours: 10 days/15:00 hours");
+        System.out.println(" ");
+        System.out.println("Week 6");
+        System.out.println("Code Time-in, Time-out Calculation (Function): 2024/02/26 02:00");
+        System.out.println("Code Break Time Calculation (Function): 2024/02/27 03:00");
+        System.out.println("Code Overall Time Calculation in 1 Day (Function): 2024/02/29 02:00");
+        System.out.println("Code Overall Time Calculation in 1 Week (Function): 2024/03/02 03:00");
+        System.out.println("Total worked day/hours: 6 days/10:00 hours ");
+        System.out.println(" ");
+        System.out.println("Week 7");
+        System.out.println("Code Double Pay, OT, RDOT (Function): 2024/03/03 02:00");
+        System.out.println("Code Gross Wage in 1 Week (Total Hrs * Sal Rate) (Function): 2024/03/05 03:00 ");
+        System.out.println("Total worked day/hours: 3 days/05:00 hours ");
+        System.out.println(" ");
+        System.out.println("Week 8 ");
+        System.out.println("Code Add All Gov't Mandated Deductions (Function): 2024/03/06 03:00");
+        System.out.println("Code Subtract Total Weekly Wage, Gov't Mandated Deductions (Function): 2024/03/07 02:00 ");
+        System.out.println("Total worked day/hours: 2 days/05:00 hours ");
+        System.out.println(" ");
+        System.out.println("Week 9-10 ");
+        System.out.println("Testing and Revision: ");
+        System.out.println(" ");
+        System.out.println("Week 11 ");
+        System.out.println("Submission of Final Output: ");
         System.out.println(" ");
         System.out.println("********************************************");
                 
